133f6bea7dfb45ab2b835de4230590850c5ab82a
Mariany Silva <mariany.silva@trustedhealth.com>
2024-01-23 10:41:51 -0300

    dbtlearn tests: add singular tests

---
+++ dbtlearn/tests/consistent_create_at.sql
@@ -0,0 +1,7 @@
+SELECT
+    *
+FROM
+    {{ ref('dim_listings_cleansed') }} listings
+    INNER JOIN {{ ref('fct_reviews') }} reviews USING (listing_id)
+WHERE
+    listings.created_at >= reviews.review_date
